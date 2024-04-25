4f476f883eea555d63e6aef8faf101522317a993
wiem77 <wiembatti77@gmail.com>
2024-04-25 15:11:11 +0200

    commit

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,19 +1,16 @@
 // list to add ESM to ignore
 const esModules = ['uuid'].join('|');
 
-
 module.exports = {
-  transform: {}, // Assurez-vous que aucun transform n'est utilisé pour les fichiers JS/MJS
   testEnvironment: 'node',
-  testMatch: [
-    '**/tests/*.[jt]s?(x)',
-    '**/?(*.)+(spec|test).c[tj]s?(x)',
-    '**/tests/**/?(*.)+(spec|test).cjs'  // Ajout pour inclure spécifiquement les fichiers .mjs dans le dossier tests
+  reporters: [
+    'default', // Utilise le reporter standard
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test_results', // Dossier où les résultats seront enregistrés
+        outputName: 'junit.xml', // Nom du fichier de sortie
+      },
+    ],
   ],
-  transform: {},
-  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
-  moduleNameMapper: {
-      // '^uuid$': '<rootDir>/node_modules/uuid/dist/index.js',
-      '^uuid$': require.resolve('uuid'),
-  }
 };
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,10 +1,24 @@
-// Importations nécessaires avec CommonJS
 const request = require('supertest');
-const app = require('../backend/server');  // Assurez-vous que cela pointe vers votre fichier serveur correctement configuré
+const app = require('../backend/server');
+let server;
+
+beforeAll((done) => {
+  server = app.listen(5001, () => {
+    console.log('Test server running on port 5001');
+    done();
+  });
+});
+
+afterAll((done) => {
+  server.close(() => {
+    console.log('Test server closed');
+    done();
+  });
+});
 
 describe('API Tests', () => {
   it('GET /contacts - should return all contacts', async () => {
-    const response = await request(app).get('/contacts');
+    const response = await request(server).get('/contacts');
     expect(response.statusCode).toBe(200);
     expect(response.body).toBeInstanceOf(Array);
   });
@@ -13,10 +27,10 @@ describe('API Tests', () => {
     const newContact = {
       name: 'John Doe',
       email: 'john@example.com',
-      phone: '1234567890'
+      phone: '1234567890',
     };
-    
-    const response = await request(app).post('/contacts').send(newContact);
+
+    const response = await request(server).post('/contacts').send(newContact);
     expect(response.statusCode).toBe(201);
     expect(response.body).toMatchObject(newContact);
   });
